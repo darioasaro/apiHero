db90c7c75a54ae02e2063ef8aade9aaa8f68dc00
MauroYini <mauro8792yini@gmail.com>
2020-01-28 11:40:44 -0300

    cambio de nombre

--- data/data.js
+++ data/data.js
@@ -1,5 +1,14 @@
 //----------------------------------------///
-
+let requestURL = 'https://www.superheroapi.com/api.php/10214201650550258/22/biography';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        const planetas = request.response;
+        console.log(planetas);
+        
+      }
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 //----------------------------------------/////
 // Creamos los modelos
-
+/* unction Planet(climate, diameter, gravity, name, orbital_period, population, rotation_period, surface_water, terrain){
+    
+    
+} */
 
 
 
